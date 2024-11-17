8ff6f129d6205b8402efe5ca5da554a1e9a2db50
S571495 <ys.sara2068@gmail.com>
2024-11-17 17:36:35 -0600

    Created react screen

---
+++ pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ConnectWalletPage from './pages/ConnectWalletPage';
+ 
+function App() {
+  return (
+    <div className="App">
+      <ConnectWalletPage></ConnectWalletPage>
+    </div>
+  );
+}
+ 
+export default App;
